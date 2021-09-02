4331045e3b631fb7acca59ab7490fb18765364e7
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-09-02 16:46:57 -0600

    Update Test
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/features/stations/index_spec.rb
+++ spec/features/stations/index_spec.rb
@@ -3,9 +3,32 @@ require 'rails_helper'
 RSpec.describe "As a guest user" do
   describe "I visit the stations index page" do
     it "displays each station with corresponding attributes" do
-      visit "/stations?location=denver,co"
+      @station1 =  {
+                      "name": "Ideal Market Capitol Hill",
+                      "distance": 0.59729,
+                      "status": "Available",
+                      "hours": "24 hours daily",
+                      "ev_network": "eVgo Network",
+                      "street_address": "900 E 11th Ave",
+                      "city": "Denver",
+                      "state": "CO",
+                      "zip_code": "80218"
+                    }
       
+      visit "/stations?location=denver,co"
+                      
       expect(page).to have_content("Nearest Stations")
+      within("#station-#{@station1.id}") do
+        expect(page).to have_content(@station1[:name])
+        expect(page).to have_content(@station1[:address])
+        expect(page).to have_content(@station1[:city])
+        expect(page).to have_content(@station1[:state])
+        expect(page).to have_content(@station1[:zip_code])
+        expect(page).to have_content(@station1[:status])
+        expect(page).to have_content(@station1[:hours])
+        expect(page).to have_content(@station1[:ev_network])
+        expect(page).to have_content(@station1[:distance])
+      end
     end
   end
 end
