5ff611c0c4e907292c306ce1178111a2b3ffb4fe
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-09-03 17:04:05 -0600

    Create station service
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

---
+++ app/services/station_service.rb
@@ -0,0 +1,8 @@
+class StationService
+  def self.get_stations(location)
+    response = Faraday.get("https://ev-station-finder-backend.herokuapp.com/api/v1/stations?location=#{location}")
+    # response = Faraday.get("http://localhost:3000/api/v1/stations?location=#{location}")
+
+    body = JSON.parse(response.body, symbolize_names: true)
+  end
+end
