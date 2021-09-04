8eec32bdf6b7f04084469a6c09fef07fa8ff61d1
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-09-04 11:17:49 -0600

    Create facade spec
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

---
+++ spec/facades/station_facade_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe StationFacade do
+  describe "::get_stations" do
+    it "should return an array of station objects", :vcr do
+      location = "Denver, CO"
+      result = StationFacade.get_stations(location)
+
+      expect(result).to be_a(Array)
+      expect(result[0]).to be_a Station
+    end
+  end
+  
+  describe "::get_station" do
+    xit "should return an array of station objects", :vcr do
+      id = 152087
+      result = StationFacade.get_station(id)
+
+      expect(result).to be_a StationDetails
+    end
+  end
+end
