10e42d6d8d27952e2e5a64d8bf0214de492d7fa5
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-12 17:32:35 -0400

    Add show action to stations controller
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,4 +2,8 @@ class StationsController < ApplicationController
   def index
     @stations = StationFacade.get_stations(params[:location])
   end
+
+  def show
+    @station = StationFacade.get_station(params[:api_id])
+  end
 end
