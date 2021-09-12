27b304e9ad3afd3beb120e458917e3cd8e3d4b97
Diana Buffone <diana.pamelaet@gmail.com>
2021-09-12 18:04:40 -0400

    Refactor get stations call for station service
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -1,11 +1,20 @@
 class StationService
   def self.get_stations(location)
-    response = Faraday.get("https://ev-station-finder-backend.herokuapp.com/api/v1/stations?location=#{location}")
+    response = conn.get("/api/v1/stations") do |faraday|
+      faraday.params['location'] = location
+    end
     # response = Faraday.get("http://localhost:3000/api/v1/stations?location=#{location}")
-
-    body = JSON.parse(response.body, symbolize_names: true)
+    parse_json(response)
   end
 
-  def self.get_station(api_id)  
+  def self.get_station(api_id)
+  end
+
+  def self.conn
+    Faraday.new(url: "https://ev-station-finder-backend.herokuapp.com")
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
