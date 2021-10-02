b5d88ef07912d0080ad3f96648eaff2e8ab4de42
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-02 10:57:31 -0600

    Create welcome spec
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome page' do
-  describe 'Has a resgistration button' do
+  before do
+    visit root_path
+  end
+  
+  describe 'When I visit the welcome page' do
+    it "I see a welcome message and a description of the application" do
+      welcome_message = "Welcome to EV Station Finder"
+      description = "Search for nearby EV charging stations and get pertinent information about them such as a 10 hour forecast and driving directions from your set location. Register for an account to save your favorite stations"
+                     
+      expect(page).to have_content(welcome_message)
+      expect(page).to have_content(description)
+    end
+    
+    it "I can see the registration button for new users" do
+      expect(page).to have_button("Sign Up")
+    end
+    
+    it "I can see the login button for registered users" do
+      expect(page).to have_button("Log In")
+    end
   end 
 end
