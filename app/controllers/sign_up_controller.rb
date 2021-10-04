b69683b44438cce26345f4ca27aea4d2f12de2c8
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-04 17:08:48 -0600

    Add create action
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,4 +1,22 @@
 class SignUpController < ApplicationController
   def new
   end
+  
+  def create
+    #TODO call backend to create user
+    # if password != password_confirmation
+    #   render ____
+    # else
+    #   #@user = UserFacade.create_user(params)
+    #   #redirect_to dashboard_path
+    # end
+  end
+  
+  private
+  
+  def user_details
+    params.permit(:first_name, :last_name, :email, :street_address, :city, :state, :zip_code, :password)
+  end
 end
+
+
