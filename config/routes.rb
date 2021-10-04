4626208c7685c1b76a0fe986ac2e6433bb05c1a8
Diana Buffone <diana.pamelaet@gmail.com>
2021-10-04 17:49:12 -0400

    Begin implementing sign up spec feature
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   resources :stations, only: [:index, :show]
-  get "/signup", to: "users#new"
+  get "/signup", to: "sign_up#new"
+  # post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
 end
