dcf905a80ff3b8d3fbd6102b9bb5e52b7c57fe55
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-04 17:06:18 -0600

    Add sign up routes
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   resources :stations, only: [:index, :show]
-  get "/signup", to: "sign_up#new"
-  # post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
+  get "/sign_up", to: "sign_up#new"
+  post "/sign_up", to: "sign_up#create"
+  post "/log_in", to: "log_in#create"
 end
