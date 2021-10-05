9179d08833878ab54ef1fee838e0cccab1baabec
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-05 16:38:18 -0600

    Update controller
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -7,15 +7,16 @@ class SignUpController < ApplicationController
     # if password != password_confirmation
     #   render ____
     # else
-    #   #@user = UserFacade.create_user(params)
-    #   #redirect_to dashboard_path
+    @user = UserFacade.create_user(user_params)
+    #   #redirect_to dashboard_path(@user)
     # end
   end
   
   private
   
-  def user_details
-    params.permit(:first_name, :last_name, :email, :street_address, :city, :state, :zip_code, :password)
+  def user_params
+    permitted_params = params.permit(:first_name, :last_name, :email, :street_address, :city, :state, :zip_code, :password)
+    permitted_params.to_h || {}
   end
 end
 
