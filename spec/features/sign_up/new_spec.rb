ea62e0186c54a898f35ea8da21ca3f0020022702
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-07 16:54:27 -0600

    Add VCR and edit notes
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/features/sign_up/new_spec.rb
+++ spec/features/sign_up/new_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Sign Up Page' do
   describe 'When a user visits the sign up page' do
-    it 'Registration page should include' do
+    it 'Registration page should include', :vcr do
       visit root_path
       click_link 'Sign Up'
-      expect(current_path).to eq(sign_up_path) # TODO: change to path helper
+      expect(current_path).to eq(sign_up_path)
       fill_in :first_name, with: "fn"
       fill_in :last_name, with: "ln"
-      fill_in :email, with: "mail@email.email"
+      fill_in :email, with: "email5@email.email"
       fill_in :street_address, with: "sa"
       fill_in :city, with: "c"
       fill_in :state, with: "s"
@@ -16,9 +16,9 @@ RSpec.describe 'Sign Up Page' do
       fill_in :password, with: "pw"
       fill_in :password_confirmation, with: "pw"
       click_button "Sign Up"
-
-      expect(current_path).to eq('/dashboard')
-      expect(new_user.first_name).to eq('fn')
+      #Need to figure out a way to get user_id to test. Mock and stub?
+      expect(current_path).to eq(dashboard_path(session[:user_id]))
+      # expect(new_user.first_name).to eq('fn')
     end
   end
 end
