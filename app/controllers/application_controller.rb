3abcdfffadf2295de85ed6881b97b3eb5e2841e7
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-10-08 16:48:39 -0600

    Add authorization as a placeholder
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authorize 
-  protect_from_forgery with: :exception #TODO: Study this code
-
-  private
-
-  def authorize
-    redirect_to root_path, notice: 'Please Log In' unless User.find_by(id: session[:user_id]) #TODO Test Authorization
-  end
+  # before_action :authorize 
+  # protect_from_forgery with: :exception #TODO: Study this code
+  # 
+  # private
+  # 
+  # def authorize
+  #   redirect_to root_path, notice: 'Please Log In' unless User.find_by(id: session[:user_id]) #TODO Test Authorization
+  # end
 end
