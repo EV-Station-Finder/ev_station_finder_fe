927c05b17e637466e940c8edc120288810e75004
Diana Buffone <diana.pamelaet@gmail.com>
2021-11-03 15:07:28 -0400

    Remove pry
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -5,7 +5,7 @@ class UserService
       faraday.params['last_name'] = user_params[:last_name]
       faraday.params['email'] = user_params[:email]
       faraday.params['street_address'] = user_params[:street_address]
-      faraday.params['city'] = user_params[:city]      
+      faraday.params['city'] = user_params[:city]
       faraday.params['state'] = user_params[:state]
       faraday.params['zip_code'] = user_params[:zip_code]
       faraday.params['password'] = user_params[:password]
@@ -16,15 +16,14 @@ class UserService
     # faraday.params = user_params.to_json
     parse_json(response)
   end
-  
+
   def self.get_user(user_token)
     response = conn.get("/api/v1/users") do |faraday|
       faraday.params['token'] = user_token
     end
     parse_json(response)
-    require "pry";binding.pry
   end
-  
+
   def self.log_in_user(user_params)
     response = conn.post("/api/v1/sessions") do |faraday|
       faraday.params['email'] = user_params[:email]
