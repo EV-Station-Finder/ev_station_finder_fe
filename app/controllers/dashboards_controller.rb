2070ce994518a5f72faf4b6c5fa1111ff6d76ea8
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-11-08 13:50:37 -0700

    Update controller
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,9 @@
 class DashboardsController < ApplicationController
   def show
-    @user = UserService.get_user(session[:token])
-    user_address = "#{@user.street_address} #{@user.city}, #{@user.state} #{@user.zip_code}"
-    @nearest_stations = StationFacade.get_stations(user_address)
+    user = UserService.get_user(session[:token])
+    @user_info = user[:data][:attributes]
+    @user_address = "#{@user_info[:street_address]} #{@user_info[:city]}, #{@user_info[:state]} #{@user_info[:zip_code]}"
+    @nearest_stations = StationFacade.get_stations(@user_address)
     @favorite_stations = UserFacade.get_favorite_stations(session[:token])
   end
 end
