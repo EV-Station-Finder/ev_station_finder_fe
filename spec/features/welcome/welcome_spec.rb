7915ce1297b482b3cce9c30ad6ce6dc568dda324
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-11-09 11:55:15 -0700

    Remove VCR
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Welcome page' do
   end
 
   describe 'When I visit the welcome page' do
-    it "I see a welcome message and a description of the application", :vcr  do
+    it "I see a welcome message and a description of the application"  do
       welcome_message = "Welcome to EV Station Finder"
       description = "Search for nearby EV charging stations and get pertinent information about them such as a 10 hour forecast and driving directions from your set location. Register for an account to save your favorite stations"
 
@@ -14,18 +14,18 @@ RSpec.describe 'Welcome page' do
       expect(page).to have_content(description)
     end
 
-    it "I can see the registration button for new users", :vcr  do
+    it "I can see the registration button for new users"  do
       expect(page).to have_link("Sign Up")
     end
 
-    it "I can see the login button for registered users", :vcr  do
+    it "I can see the login button for registered users"  do
       expect(page).to have_button("Log In")
     end
 
     describe 'When I fill in the login form' do
       it "I can see an area to enter my login information", :vcr  do
-        fill_in :email, with: "whatever55@example.com"
-        fill_in :password, with: "password"
+        fill_in :email, with: "wizard@hogwarts.com"
+        fill_in :password, with: "verysecurepassword"
         click_button "Log In"
         expect(current_path).to eq('/dashboard')
       end
