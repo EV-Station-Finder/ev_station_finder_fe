0583d6a8453c709911333bc9101cf222ffdba589
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-11-10 13:50:24 -0700

    Add log out route
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "/sign_up", to: "users#new"
   post "/sign_up", to: "users#create"
   post "/log_in", to: "sessions#create"
+  delete "/log_out", to: "sessions#destroy"
   resource :dashboard, only: :show
 end
