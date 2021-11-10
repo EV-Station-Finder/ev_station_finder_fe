99c1da1c5040395f2a8624e1edb3f3552bc06b92
Diana Buffone <diana.pamelaet@gmail.com>
2021-11-10 14:14:53 -0500

    Implement sad path tests for logging in a user
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/log_in_controller.rb
+++ app/controllers/log_in_controller.rb
@@ -1,4 +1,4 @@
-class LogInController < ApplicationController  
+class LogInController < ApplicationController
   def create
     user = UserService.log_in_user(user_params)
     if user[:data]
@@ -6,18 +6,14 @@ class LogInController < ApplicationController
       redirect_to dashboard_path
     else
       flash[:errors] = user[:errors]
-      render "welcome#index"
+      redirect_to root_path
     end
   end
-  
+
   private
-  
+
   def user_params
     permitted_params = params.permit(:email, :password)
     permitted_params.to_h || {}
   end
 end
-
-
-
-
