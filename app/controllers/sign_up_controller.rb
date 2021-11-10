5d4e957f636aae5fc018c1495206802aaca2bf0c
Diana Buffone <diana.pamelaet@gmail.com>
2021-11-10 13:52:10 -0500

    Update with user token instead of user id
    Co-Authored-By: Alexander Brueck <brueck1988@users.noreply.github.com>

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,24 +1,22 @@
 class SignUpController < ApplicationController
   def new
   end
-  
+
   def create
     user = UserService.create_user(user_params)
     if user[:data]
-      session[:user_id] = user[:data][:id]
-      redirect_to dashboard_path(user[:data][:id])
+      session[:token] = user[:data][:token]
+      redirect_to dashboard_path
     else
       flash[:errors] = user[:errors]
       render :new
     end
   end
-  
+
   private
-  
+
   def user_params
     permitted_params = params.permit(:first_name, :last_name, :email, :street_address, :city, :state, :zip_code, :password)
     permitted_params.to_h || {}
   end
 end
-
-
