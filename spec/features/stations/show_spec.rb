ba95b14ddd88109a784f8408f44c1d784b129052
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-11-18 13:55:39 -0700

    Add tests for favoriting station
    Co-Authored-By: Diana Buffone <Diana20920@users.noreply.github.com>

--- spec/features/stations/show_spec.rb
+++ spec/features/stations/show_spec.rb
@@ -1,111 +1,159 @@
 require 'rails_helper'
 
 RSpec.describe "Station Show" do
-  describe "As a guest user, I visit the stations show page" do
-    it "displays a single station attributes", :vcr do
-      @station1 =  {
-            "name": "Casey's General Store",
-            "api_id": 152087,
-            "status": "Available",
-            "hours": "24 hours daily",
-            "ev_network": nil,
-            "street_address": "2375 St Andrews Dr",
-            "city": "Altoona",
-            "state": "WI",
-            "zip_code": "54720",
-            "accepted_payments": [
-                "American Express",
-                "Discover",
-                "MasterCard",
-                "Visa"
-            ],
-            "hourly_weather": [
-                {
-                    "time": "21:14",
-                    "temperature": 32,
-                    "conditions": "overcast clouds",
-                    "icon": "04d"
-                },
-                {
-                    "time": "21:00",
-                    "temperature": 75,
-                    "conditions": "broken clouds",
-                    "icon": "n15"
-                },
-                {
-                    "time": "22:00",
-                    "temperature": 70.11,
-                    "conditions": "few clouds",
-                    "icon": "02d"
-                },
-                {
-                    "time": "23:00",
-                    "temperature": 70.14,
-                    "conditions": "scattered clouds",
-                    "icon": "03d"
-                },
-                {
-                    "time": "00:00",
-                    "temperature": 66.29,
-                    "conditions": "broken clouds",
-                    "icon": "04d"
-                },
-                {
-                    "time": "01:00",
-                    "temperature": 61.45,
-                    "conditions": "clear sky",
-                    "icon": "01n"
-                },
-                {
-                    "time": "02:00",
-                    "temperature": 57.52,
-                    "conditions": "clear sky",
-                    "icon": "01n"
-                },
-                {
-                    "time": "03:00",
-                    "temperature": 55.56,
-                    "conditions": "clear sky",
-                    "icon": "01n"
-                },
-                {
-                    "time": "04:00",
-                    "temperature": 54.3,
-                    "conditions": "clear sky",
-                    "icon": "01n"
-                },
-                {
-                    "time": "05:00",
-                    "temperature": 53.65,
-                    "conditions": "clear sky",
-                    "icon": "01n"
-                }
-            ]
+  describe "HAPPY PATH" do
+    let(:station1) { {
+      "name": "Casey's General Store",
+      "api_id": 152087,
+      "status": "Available",
+      "hours": "24 hours daily",
+      "ev_network": nil,
+      "street_address": "2375 St Andrews Dr",
+      "city": "Altoona",
+      "state": "WI",
+      "zip_code": "54720",
+      "accepted_payments": [
+        "American Express",
+        "Discover",
+        "MasterCard",
+        "Visa"
+      ],
+      "hourly_weather": [
+        {
+          "time": "20:49",
+          "temperature": 30,
+          "conditions": "light snow",
+          "icon": "04d"
+        },
+        {
+          "time": "21:00",
+          "temperature": 75,
+          "conditions": "broken clouds",
+          "icon": "n15"
+        },
+        {
+          "time": "22:00",
+          "temperature": 70.11,
+          "conditions": "few clouds",
+          "icon": "02d"
+        },
+        {
+          "time": "23:00",
+          "temperature": 70.14,
+          "conditions": "scattered clouds",
+          "icon": "03d"
+        },
+        {
+          "time": "00:00",
+          "temperature": 66.29,
+          "conditions": "broken clouds",
+          "icon": "04d"
+        },
+        {
+          "time": "01:00",
+          "temperature": 61.45,
+          "conditions": "clear sky",
+          "icon": "01n"
+        },
+        {
+          "time": "02:00",
+          "temperature": 57.52,
+          "conditions": "clear sky",
+          "icon": "01n"
+        },
+        {
+          "time": "03:00",
+          "temperature": 55.56,
+          "conditions": "clear sky",
+          "icon": "01n"
+        },
+        {
+          "time": "04:00",
+          "temperature": 54.3,
+          "conditions": "clear sky",
+          "icon": "01n"
+        },
+        {
+          "time": "05:00",
+          "temperature": 53.65,
+          "conditions": "clear sky",
+          "icon": "01n"
         }
+      ]
+    } }
+    
+    let(:station2) { {
+      "name": "G&M OIL CHEVRON #111",
+      "api_id": 182990
+      } }
+      
+    describe "As a guest user, I visit the stations show page" do
+      it "displays a single station attributes", :vcr do
+        visit station_path(station1[:api_id])
 
-      visit "/stations/#{@station1[:api_id]}"
+        expect(page).to have_content(station1[:name])
+        expect(page).to have_content("Address: #{station1[:street_address]}, #{station1[:city]}, #{station1[:state]} #{station1[:zip_code]}")
+        expect(page).to have_content("Status: #{station1[:status]}")
+        expect(page).to have_content("Hours: #{station1[:hours]}")
+        # expect(page).to have_content("EV Network: #{station1[:ev_network]}")
+        expect(page).to have_content("10 Hour Forecast")
+        within("#forecast-0") do
+          expect(page).to have_content(station1[:hourly_weather][0][:time])
+          expect(page).to have_content(station1[:hourly_weather][0][:temperature].to_i)
+          expect(page).to have_content(station1[:hourly_weather][0][:conditions])
+          # expect(page).to have_xpath("https://openweathermap.org/img/wn/#{station1[:hourly_weather][0][:icon]}@2x.png")
+          expect(page).to have_css("img[src*='https://openweathermap.org/img/wn/#{station1[:hourly_weather][0][:icon]}@2x.png']")
+        end
+        within("#accepted_payment-0") do
+          expect(page).to have_content("American Express")
+        end
+      end
 
-      expect(page).to have_content(@station1[:name])
-      expect(page).to have_content("Address: #{@station1[:street_address]}, #{@station1[:city]}, #{@station1[:state]} #{@station1[:zip_code]}")
-      expect(page).to have_content("Status: #{@station1[:status]}")
-      expect(page).to have_content("Hours: #{@station1[:hours]}")
-      # expect(page).to have_content("EV Network: #{@station1[:ev_network]}")
-      expect(page).to have_content("10 Hour Forecast")
-      within("#forecast-0") do
-        expect(page).to have_content(@station1[:hourly_weather][0][:time])
-        expect(page).to have_content(@station1[:hourly_weather][0][:temperature].to_i)
-        expect(page).to have_content(@station1[:hourly_weather][0][:conditions])
-        # expect(page).to have_xpath("https://openweathermap.org/img/wn/#{@station1[:hourly_weather][0][:icon]}@2x.png")
-        expect(page).to have_css("img[src*='https://openweathermap.org/img/wn/#{@station1[:hourly_weather][0][:icon]}@2x.png']")
+      it "does not display the logout link", :vcr do
+        visit station_path(station1[:api_id])
+        expect(page).to_not have_link("Log Out")
+        expect(page).to have_link("Log In")
       end
-      within("#accepted_payment-0") do
-        expect(page).to have_content("American Express")
-      end
+      
+      it "does not display favorite station link"
+      
     end
-
-    it "does not display the logout link", :vcr do
-      visit "/stations/152087"
-      expect(page).to_not have_link("Log Out")
+    describe "As a logged in user, I visit the stations show page" do
+      before :each do
+        visit root_path
+        fill_in :email, with: "wizard@hogwarts.com"
+        fill_in :password, with: "verysecurepassword"
+        click_button 'Log In'
+        click_link "G&M OIL CHEVRON #111"
+      end
+      
+      it "display link to favorite a station", :vcr do
+        expect(current_path).to eq(station_path(station2[:api_id]))
+        click_link("Favorite Station")
+        expect(current_path).to eq(station_path(station2[:api_id]))
+        expect(page).to have_content("#{station2[:name]} has been added to your favorite stations")
+        
+        visit dashboard_path
+        
+        within("#favorite-station-2") do
+          expect(page).to have_content(station2[:name])
+        end
+        # TODO: Remove staiton 2 from favorites once point has been created
+        # click_link("Unfavorite Station")
+      end
+      
+      xit "display link to unfavorite a station", :vcr do
+        click_link("Unfavorite Station")
+        expect(current_path).to eq(station_path(station1[:api_id]))
+        expect(page).to have_content("#{station1[:name]} has been added to your favorite stations")
+        click_link("Favorite Station")
+      end
+      
+      
+      it "displays the logout link", :vcr do
+        expect(page).to have_link("Log Out")
+        expect(page).to_not have_link("Log In")
+      end
     end
   end
 end
