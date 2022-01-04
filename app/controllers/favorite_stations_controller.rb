083deb207988a201a60de58cd1a1408cb4bbf142
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2022-01-04 11:21:42 -0700

    Add method

--- app/controllers/favorite_stations_controller.rb
+++ app/controllers/favorite_stations_controller.rb
@@ -5,4 +5,11 @@ class FavoriteStationsController < ApplicationController
     flash[:notice] = "#{params[:station_name]} has been added to your favorite stations"
     redirect_to station_path(params[:api_id])
   end
+  
+  def destroy
+    # guest users without session token will be redirected to the root path by the authorize method
+    StationFacade.remove_favorite_station(params[:api_id], session[:token])
+    flash[:notice] = "#{params[:station_name]} has been removed from your favorite stations"
+    redirect_to station_path(params[:api_id])
+  end
 end
